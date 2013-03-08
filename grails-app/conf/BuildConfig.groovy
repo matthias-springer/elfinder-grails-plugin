72830ed706f16cdfa614d2736850ea960aa05786
Burt Beckwith <burt@burtbeckwith.com>
2013-03-08 10:53:54 -0500

    cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,29 +1,44 @@
 grails.server.port.http = 8081
 
+grails.project.work.dir = 'target'
+
 grails.project.dependency.resolution = {
-    inherits("global") {}
+
+    inherits 'global'
+    log 'warn'
 
     repositories {
         grailsCentral()
+        mavenLocal()
         mavenCentral()
     }
 
     dependencies {
-        compile 'org.apache.commons:commons-lang3:3.0'
-        compile 'commons-fileupload:commons-fileupload:1.2.1'
-        compile 'commons-io:commons-io:2.3'
-        compile 'com.mortennobel:java-image-scaling:0.8.5'
-        compile 'eu.medsea.mimeutil:mime-util:2.1.3'
+        compile 'org.apache.commons:commons-lang3:3.0', {
+            excludes 'easymock', 'junit'
+        }
+        compile 'commons-fileupload:commons-fileupload:1.2.1', {
+            excludes 'commons-io', 'junit', 'portlet-api', 'servlet-api'
+        }
+        compile 'commons-io:commons-io:2.3', {
+            excludes 'junit'
+        }
+        compile 'com.mortennobel:java-image-scaling:0.8.5', {
+            excludes 'filters', 'junit'
+        }
+        compile 'eu.medsea.mimeutil:mime-util:2.1.3', {
+            excludes 'junit', 'log4j', 'slf4j-api', 'slf4j-log4j12'
+        }
         compile 'org.json:json:20090211'
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.0.3",
-              ":rest-client-builder:1.0.2") {
+        build(":release:2.2.1", ":rest-client-builder:1.0.3") {
             export = false
         }
 
-        runtime ':tomcat:2.1.1'
+        build ':tomcat:2.1.1', {
+            export = false
+        }
     }
 }
